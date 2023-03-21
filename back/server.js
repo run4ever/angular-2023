757ddd7c3bcffb01a09cc1b10f4f059f457f2ede
Fabien Laurette <fabien.laurette@gmail.com>
2023-03-21 14:46:46 +0100

    ajout serve index

--- back/server.js
+++ back/server.js
@@ -1,6 +1,7 @@
 console.log('About to start a server');
 
 const express = require('express');
+const serveIndex = require('serve-index');
 const app = express();
 const port = 3000;
 
@@ -8,6 +9,9 @@ app.use((req, res, next) => {
   console.log('url:', req.path);
   next();
 });
+
+app.use(express.static('.'));
+app.use(serveIndex('.', { icons: true }));
 
 app.get('/', (req, res) => {
   res.send('Hello World!');
