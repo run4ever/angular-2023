af11da30d40d38c5d954b557cb571f49715aa427
Fabien Laurette <fabien.laurette@gmail.com>
2023-03-22 17:27:50 +0100

    boucle articles avec objet interface

--- front/src/app/stock/stock.component.ts
+++ front/src/app/stock/stock.component.ts
@@ -1,10 +1,15 @@
 import { Component } from '@angular/core';
+import { Article } from '../interfaces/article';
 
 @Component({
   selector: 'app-stock',
   templateUrl: './stock.component.html',
-  styleUrls: ['./stock.component.scss']
+  styleUrls: ['./stock.component.scss'],
 })
 export class StockComponent {
-
+  articles: Article[] = [
+    { id: 'a1', name: 'Tournevis', price: 3.99, qty: 12 },
+    { id: 'a2', name: 'Marteau', price: 9.99, qty: 15 },
+    { id: 'a3', name: 'Pelle', price: 19.99, qty: 2 },
+  ];
 }
