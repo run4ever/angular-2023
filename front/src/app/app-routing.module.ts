3a9392551b32f11f72c4fd28e4f4817b50b2df22
Fabien Laurette <fabien.laurette@gmail.com>
2023-03-22 14:21:10 +0100

    routing config

--- front/src/app/app-routing.module.ts
+++ front/src/app/app-routing.module.ts
@@ -1,10 +1,17 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { HomeComponent } from './routes/home/home.component';
+import { LegalComponent } from './routes/legal/legal.component';
 
-const routes: Routes = [];
+//ne pas mettre le "/" du debut
+const routes: Routes = [
+  { path: '', component: HomeComponent },
+  { path: 'legal', component: LegalComponent },
+  { path: '**', component: HomeComponent },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
