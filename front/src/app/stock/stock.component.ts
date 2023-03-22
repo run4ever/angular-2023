ae938ddbcfc1ae1b6eb789e0dc3a0893195a6b87
Fabien Laurette <fabien.laurette@gmail.com>
2023-03-22 17:36:01 +0100

    extension de rangement des methodes

--- front/src/app/stock/stock.component.ts
+++ front/src/app/stock/stock.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, OnDestroy } from '@angular/core';
 import { Article } from '../interfaces/article';
 
 @Component({
@@ -6,10 +6,14 @@ import { Article } from '../interfaces/article';
   templateUrl: './stock.component.html',
   styleUrls: ['./stock.component.scss'],
 })
-export class StockComponent {
+export class StockComponent implements OnDestroy {
   articles: Article[] = [
     { id: 'a1', name: 'Tournevis', price: 3.99, qty: 12 },
     { id: 'a2', name: 'Marteau', price: 9.99, qty: 15 },
     { id: 'a3', name: 'Pelle', price: 19.99, qty: 2 },
   ];
+
+  ngOnDestroy(): void {
+    console.log('bye bybe');
+  }
 }
