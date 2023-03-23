1fe7577f8b597f77d73f8e7f7538839288d9fd1d
Fabien Laurette <fabien.laurette@gmail.com>
2023-03-23 12:08:07 +0100

    utilisation observable

--- front/src/app/services/article.service.ts
+++ front/src/app/services/article.service.ts
@@ -1,5 +1,5 @@
 import { Injectable } from '@angular/core';
-import { BehaviorSubject } from 'rxjs';
+import { BehaviorSubject, distinctUntilChanged, Observable } from 'rxjs';
 import { Article } from '../interfaces/article';
 
 @Injectable({
@@ -7,11 +7,15 @@ import { Article } from '../interfaces/article';
 })
 export class ArticleService {
   //on definit l'observable (syntaxe $)
-  articles$ = new BehaviorSubject<Article[]>([
+  private articles$ = new BehaviorSubject<Article[]>([
     { id: 'a1', name: 'Tournevis', price: 3.99, qty: 12 },
     { id: 'a2', name: 'Marteau', price: 9.99, qty: 15 },
     { id: 'a3', name: 'Pelle', price: 19.99, qty: 2 },
   ]);
+
+  getArticles(): Observable<Article[]> {
+    return this.articles$.pipe(distinctUntilChanged());
+  }
 
   constructor() {
     //on ajoute un element au bout de 2 secondes
