c8b398f006889a800281e4113e102ba85b8e2597
Fabien Laurette <fabien.laurette@gmail.com>
2023-03-24 14:17:51 +0100

    pipe currency

--- front/src/app/app.module.ts
+++ front/src/app/app.module.ts
@@ -1,4 +1,4 @@
-import { NgModule } from '@angular/core';
+import { DEFAULT_CURRENCY_CODE, LOCALE_ID, NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
 import { HttpClientModule } from '@angular/common/http';
@@ -9,6 +9,10 @@ import { HomeComponent } from './routes/home/home.component';
 import { LegalComponent } from './routes/legal/legal.component';
 import { ArticleService } from './services/article.service';
 import { BackArticleService } from './services/back-article.service';
+import { registerLocaleData } from '@angular/common';
+import localeFr from '@angular/common/locales/fr';
+
+registerLocaleData(localeFr, 'fr');
 
 @NgModule({
   declarations: [AppComponent, HomeComponent, LegalComponent],
@@ -22,7 +26,11 @@ import { BackArticleService } from './services/back-article.service';
 
   //dependancy injections : au lieu d'utiliser la classe ArticleService on va utiliser BackArticleService
   //dans la vraie vie on modifierait directement la classe ArticleService mais là c'est à but pédagogique
-  providers: [{ provide: ArticleService, useClass: BackArticleService }],
+  providers: [
+    { provide: DEFAULT_CURRENCY_CODE, useValue: 'EUR' },
+    { provide: LOCALE_ID, useValue: 'fr-FR' },
+    { provide: ArticleService, useClass: BackArticleService },
+  ],
 
   bootstrap: [AppComponent],
 })
