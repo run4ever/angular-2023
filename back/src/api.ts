081fbf175cce75d3213464ec1697f5a014fab1b6
Fabien Laurette <fabien.laurette@gmail.com>
2023-03-24 11:09:40 +0100

    correctif

--- back/src/api.ts
+++ back/src/api.ts
@@ -2,9 +2,9 @@ import express from 'express';
 import { Article } from './interfaces/article';
 
 const articles: Article[] = [
-  { id: 'a1', name: 'Tournevisxxx', price: 3.99, qty: 12 },
-  { id: 'a2', name: 'Marteaxxxu', price: 9.99, qty: 15 },
-  { id: 'a3', name: 'Pellxxxe', price: 19.99, qty: 2 },
+  { id: 'a1', name: 'Tournevis-back', price: 3.99, qty: 12 },
+  { id: 'a2', name: 'Marteau-back', price: 9.99, qty: 15 },
+  { id: 'a3', name: 'Pelle-back', price: 19.99, qty: 2 },
 ];
 
 const app = express.Router();
