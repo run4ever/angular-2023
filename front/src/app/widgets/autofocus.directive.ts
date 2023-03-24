7e7659b9fa8ad7dd777efc8e0247c2110bc21e0c
Fabien Laurette <fabien.laurette@gmail.com>
2023-03-24 13:59:13 +0100

    ajout directive implementation

--- front/src/app/widgets/autofocus.directive.ts
+++ front/src/app/widgets/autofocus.directive.ts
@@ -1,10 +1,15 @@
-import { Directive } from '@angular/core';
+import { Directive, ElementRef, OnInit } from '@angular/core';
 
 @Directive({
   selector: '[appAutofocus]',
 })
-export class AutofocusDirective {
-  constructor() {
+export class AutofocusDirective implements OnInit {
+  constructor(private readonly elt: ElementRef<HTMLInputElement>) {
     console.log('appAutofocus instanciated :-)');
+    //this.elt.nativeElement.focus(); ne pas mettre ici car  la patie visuelle n'est pas complètement finie (pas encore rattachée au DOM)
+  }
+
+  ngOnInit(): void {
+    this.elt.nativeElement.select();
   }
 }
